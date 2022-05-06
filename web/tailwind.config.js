5749ea5053442966b75bbd67ad640826e3c1fe5b
ivanmduarte <ivan.mduarte@live.com>
2022-05-05 22:12:30 -0300

    Finalização front Widget web
    Finalização aula 2 do nlw, feedback com todas as etapas do front implementadas.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6',
         }
+      },
+      borderRadius:{ 
+        md: '4px', //Alterando o valor de 6 para 4
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
